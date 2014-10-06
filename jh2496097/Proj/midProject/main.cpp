264fef708ae5c8ad2323c681f2b3154dd5b49597
jh2496097 <holtyguy33@yahoo.com>
2014-10-06 14:44:30 -0700

    proj
    Signed-off-by: jh2496097 <holtyguy33@yahoo.com>

--- jh2496097/Proj/midProject/main.cpp
+++ jh2496097/Proj/midProject/main.cpp
@@ -10,52 +10,88 @@
 #include <iomanip>
 #include <cctype>
 #include <cstring>
-
+#include <ctime>
 
 using namespace std;
 
-/*
- * 
- */
+//table
 const int ROWS = 18;
 const int COLS = 8;
+
+enum ObSpots {ONE = 1 , TWO = 2, THREE = 3, FOUR = 4, FIVE = 5};
+//diff objects to place on table
+const int object1 [TWO][TWO] = {1,1,1,1};
+const int object2 [ONE][THREE] = {1,1,1};
+const int object3 [FOUR][ONE] = {1,1,1,1};
+const int object4 [TWO][THREE] = {1,1,1,1,1,1};
+const int object5 [THREE][TWO] = {1,1,1,1,1,1};
+struct Objects
+{
+    //int ob1 [2][2] = {1,1,1,1};
+    //int ob2 [2][2] = {1,1,1,1};
+};
 void outputBegin ();
+int randObject ();
+int **objectNum (int num);
 void outputTbl(int **, int, int);
 int **fillGrid (int , int);
+
 void destroy (int**, int);
 
 int main(int argc, char** argv) {
-    outputBegin ();
+    //making table
     int **table = fillGrid (ROWS, COLS);
-    //fillGrid (table, ROWS);
     
-    outputTbl(table, ROWS, COLS);
-    int object [2][2] = {1,1,1,1};
     int spot;
+    int count = 0;
+    
+    for (int i=0; i < TWO;i++)
+    {
+        for(int j=0; j<TWO; j++)
+        {
+           cout << object1[i][j]  << "  ";
+           count++;
+           if (count == TWO)
+           {
+               cout <<  endl;
+               count = 0;
+           }
+        }
+    }
+    int num = 1;
+    //int **object = **objectNum (num);
+    outputBegin ();
+    outputTbl(table, ROWS, COLS);
+    int **object = objectNum (num);
     cout << "Pick a spot to place object: " ;
     cin >> spot;
-    if (spot == 5)
+    //object1
+    if (spot == 3)
     {
-        for (int i=0; i < 2;i++)
-        for(int j=0; j<2; j++)
-        table [i][spot+j] = 1;
+        for (int i=ROWS-1; i >= ROWS-TWO; i--)
+        {
+            for(int j=0; j < TWO; j++)
+            {
+                table [i][j+spot-1] = 1;
+            }
+        }
+        /*for (int i = ROWS-1; i  >=0; i--)
+        for(int j = COLS-1; j >= 0; j--)*/
     }
-    for (int i=0; i < 2;i++)
-        for(int j=0; j<2; j++)
-        cout << object[i][j]  << "  " << endl;
+    
     outputTbl(table, ROWS, COLS);
     
     destroy(table, ROWS);
-    
+    destroy (object, TWO);
     return 0;
 }
 void outputTbl(int **ptr, int rows, int cols)
 {
     int count = 0;
-    for (int i = 0 ; i <rows; i++)
+    for (int i = 0; i < ROWS; i++)
     {
         cout << "\t\t";
-        for(int j = 0; j < cols; j++)
+        for(int j = 0; j < COLS; j++)
         {
             cout << ptr[i][j] << "   ";
             count++;
@@ -120,4 +156,115 @@ void destroy(int **array,int rows)
         delete []array[i];
     }
     delete []array;
+}
+int randObject ()
+{
+    int num;
+    num = rand()%5+1;
+    return num;
+}
+/*
+ * Creating diff object to be placed in the table similiar to objects at 
+ * top with const creation using dynamic allocation here
+ */
+int **objectNum (int num)
+{
+    int count =0;
+    int **object;
+    if (num == 1)
+    {
+        object=new int*[TWO];//rows
+        //creating 2D array
+        for (int i=0; i < TWO;i++)//rows
+        {
+            object[i]=new int[TWO];//cols
+        }
+        //once created filling and outputting
+        for (int i=0; i <TWO; i++){//rows
+           for(int j=0; j <TWO; j++){//cols
+              object[i][j]= 1;
+              cout << object[i][j]  << "  ";
+              count++;
+              if (count == TWO)
+              {
+                  cout <<  endl;
+                  count = 0;
+              }
+           }
+        }
+    }
+    if (num == 2)
+    {
+        object=new int*[ONE];//rows
+        //creating 2D array
+        for (int i=0; i < THREE;i++)//rows
+        {
+            object[i]=new int[THREE];//cols
+        }
+        //once created filling and outputting
+        for (int i=0; i <ONE; i++){//rows
+           for(int j=0; j <THREE; j++){//cols
+              object[i][j]= 1;
+              cout << object1[i][j]  << "  ";
+              count++;
+              if (count == THREE)
+              {
+                  cout <<  endl;
+                  count = 0;
+              }
+           }
+        }
+    }
+    /*if (num == 3)
+    {
+        objectNum = object3;
+        for (int i=0; i < TWO;i++)
+        {
+           for(int j=0; j<TWO; j++)
+           {
+              cout << object1[i][j]  << "  ";
+              count++;
+              if (count == TWO)
+              {
+                  cout <<  endl;
+                  count = 0;
+              }
+           }
+        }
+    }
+    if (num == 4)
+    {
+        objectNum = object4;
+        for (int i=0; i < TWO;i++)
+        {
+           for(int j=0; j<TWO; j++)
+           {
+              cout << object1[i][j]  << "  ";
+              count++;
+              if (count == TWO)
+              {
+                  cout <<  endl;
+                  count = 0;
+              }
+           }
+        }
+    }
+    if (num == 5)
+    {
+        objectNum = object5;
+        for (int i=0; i < TWO;i++)
+        {
+           for(int j=0; j<TWO; j++)
+           {
+              cout << object1[i][j]  << "  ";
+              count++;
+              if (count == TWO)
+              {
+                  cout <<  endl;
+                  count = 0;
+              }
+           }
+        }
+    }*/
+    return object;
 }
