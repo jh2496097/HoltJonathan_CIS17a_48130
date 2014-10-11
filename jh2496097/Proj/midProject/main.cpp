73674225902def0dfda26c4209032e91aae3a94a
jh2496097 <holtyguy33@yahoo.com>
2014-10-10 19:31:33 -0700

    proj Signed-off-by: jh2496097 <holtyguy33@yahoo.com>

--- jh2496097/Proj/midProject/main.cpp
+++ jh2496097/Proj/midProject/main.cpp
@@ -25,64 +25,76 @@ const int object2 [ONE][THREE] = {1,1,1};
 const int object3 [FOUR][ONE] = {1,1,1,1};
 const int object4 [TWO][THREE] = {1,1,1,1,1,1};
 const int object5 [THREE][TWO] = {1,1,1,1,1,1};
-struct Objects
-{
+//struct Objects
+//{
+    //cant initialize in structures
     //int ob1 [2][2] = {1,1,1,1};
     //int ob2 [2][2] = {1,1,1,1};
-};
+//};
 void outputBegin ();
 int randObject ();
 int **objectNum (int num);
 void outputTbl(int **, int, int);
 int **fillGrid (int , int);
-
+bool isOver (int **tbl);
 void destroy (int**, int);
+void objtPlcmnt(int **tble, int spot);
 
 int main(int argc, char** argv) {
     //making table
     int **table = fillGrid (ROWS, COLS);
-    
+    bool game;
     int spot;
     int count = 0;
-    
-    for (int i=0; i < TWO;i++)
-    {
-        for(int j=0; j<TWO; j++)
-        {
-           cout << object1[i][j]  << "  ";
-           count++;
-           if (count == TWO)
-           {
-               cout <<  endl;
-               count = 0;
-           }
-        }
-    }
+    //suppose to be random num between 1 n 5
     int num = 1;
-    //int **object = **objectNum (num);
+    //begin function
     outputBegin ();
     outputTbl(table, ROWS, COLS);
-    int **object = objectNum (num);
-    cout << "Pick a spot to place object: " ;
-    cin >> spot;
-    //object1
-    if (spot == 3)
+    do
     {
-        for (int i=ROWS-1; i >= ROWS-TWO; i--)
-        {
-            for(int j=0; j < TWO; j++)
-            {
-                table [i][j+spot-1] = 1;
-            }
+        int **object = objectNum (num);
+        cout << "Pick a spot to place object (1 to 8): " ;
+        cin >> spot;
+    /*while (spot < 1 || spot > 8){//{v > 100 || v <= 0)
+        cout << "Pick a spot between 1 and 8!!!" << endl;
+        cin >> spot;
+    }*/
+    //IMPORTANT BOUNDS CHECKING AND PLACEMENT
+    //first area checks spot choice compared to column size of object
+    //rest makes sure number is 1 - 8
+        while ((spot-1) + TWO > 8 || spot < 1 || spot > 8){
+            cout << "Over Bounds will occur!!!!" << endl;
+            cout << "Cannot place there please pick another:" << endl;
+            cin >> spot;
         }
+       objtPlcmnt(table, spot);
+     //object for any spot 1 - 8 only
+       /*for (int k = 1; k < 9; k++)
+       {
+        //int realSpot =0;
+        //int rowCount =0, colCount =0;
+        //object1 only spot 6 only
+            if (spot == k)
+            {
+                for (int i=ROWS-1; i >= ROWS-TWO; i--){
+                    for(int j=0; j < TWO; j++)  {
+                        table [i][j+spot-1] = 1;
+                    }
+                }
+            }  //this is full reverse but dont need full reverse
         /*for (int i = ROWS-1; i  >=0; i--)
         for(int j = COLS-1; j >= 0; j--)*/
-    }
+        //}
     
-    outputTbl(table, ROWS, COLS);
-    
+        outputTbl(table, ROWS, COLS);
+        destroy (object, TWO);
+        //checking if lost
+        game = isOver(table);
+    }while(game == true);
     destroy(table, ROWS);
-    destroy (object, TWO);
+    
+    
     return 0;
 }
 void outputTbl(int **ptr, int rows, int cols)
@@ -105,6 +117,10 @@ void outputTbl(int **ptr, int rows, int cols)
     cout << "\t    -------------------------------------" << endl;
     cout << "Columns \t1   2   3   4   5   6   7   8     "<< endl;
 }
+/*
+ * Function fills a table with numbers by the set amount of ROWS n COLS
+ * sets each one to 0 and then returns the 2D pointer
+ */
 int **fillGrid (int ROWS, int COLS)
 {
     int **array=new int*[ROWS];
@@ -148,6 +164,9 @@ void outputBegin ()
             << "Filling a whole row will cause that row to be deleted and "
             <<endl << "points will be awarded. " << endl;
 }
+/*
+ * Deleting allocated memory for 2D arrys/ptrs
+ */
 void destroy(int **array,int rows)
 {
     //Destroy in reverse order of creation
@@ -157,6 +176,10 @@ void destroy(int **array,int rows)
     }
     delete []array;
 }
+/*
+ * rand function getting a random number between 1 and 5 in respect to 5
+ * diff objects
+ */ 
 int randObject ()
 {
     int num;
@@ -180,7 +203,9 @@ int **objectNum (int num)
             object[i]=new int[TWO];//cols
         }
         //once created filling and outputting
+        cout << "\tThis is your object : " << endl;
         for (int i=0; i <TWO; i++){//rows
+            cout << "\t";
            for(int j=0; j <TWO; j++){//cols
               object[i][j]= 1;
               cout << object[i][j]  << "  ";
@@ -202,7 +227,9 @@ int **objectNum (int num)
             object[i]=new int[THREE];//cols
         }
         //once created filling and outputting
+        cout << "\tThis is your object : " << endl  ;
         for (int i=0; i <ONE; i++){//rows
+            cout << "\t";
            for(int j=0; j <THREE; j++){//cols
               object[i][j]= 1;
               cout << object1[i][j]  << "  ";
@@ -217,20 +244,7 @@ int **objectNum (int num)
     }
     /*if (num == 3)
     {
-        objectNum = object3;
-        for (int i=0; i < TWO;i++)
-        {
-           for(int j=0; j<TWO; j++)
-           {
-              cout << object1[i][j]  << "  ";
-              count++;
-              if (count == TWO)
-              {
-                  cout <<  endl;
-                  count = 0;
-              }
-           }
-        }
+        object = object1;
     }
     if (num == 4)
     {
@@ -267,4 +281,86 @@ int **objectNum (int num)
         }
     }*/
     return object;
+}
+void spotChoice (int **tbl, int **obj, int spot, int col){
+    cout << "Enter the column you wish to place the object:" << endl;
+    cin >> spot;
+    
+    while (spot + col >= 8 ){
+        cout << "Over Bounds will occur!!!!" << endl;
+        cout << "Cannot place there please pick another:" << endl;
+        cin >> spot;
+    }
+    //object1
+    if (spot == 3)
+    {
+        for (int i=ROWS-1; i >= ROWS-TWO; i--){
+            for(int j=0; j < TWO; j++){
+                tbl [i][j+spot-1] = 1;
+            }
+        }
+        /*for (int i = ROWS-1; i  >=0; i--)
+        for(int j = COLS-1; j >= 0; j--)*/
+    }
+}
+/*
+ * check to see if a column is full and if it is then game over
+ */
+bool isOver (int **tbl)
+{
+    bool lose;
+    //for (int i =0; i < ROWS; i++){ actually dont need to check columns
+    int i = 0;
+    for (int col=0; col < COLS;col++){
+        //[0][j] because this would start from the top left of table
+        if (tbl[i][col] == 1)
+        {
+            lose = false;
+        }
+        else
+            lose = true;
+    }
+    return lose;
+}
+void objtPlcmnt(int **tble, int spot)
+{
+    int counter =0;
+    //object for any spot 1 - 8 only
+       //for (int k = 1; k < 9; k++)
+       //{
+        //object1 only spot 6 only
+            //if (spot == k)
+            int j = spot-1;
+                //starting from bottom left to top
+                for (int i =ROWS-1 ; i >= 0; i--){
+                    //for (int j = spot-1; j < COLS; j++)
+                    //{
+                        
+                    
+                    if (tble [i][j] != 0){
+                        counter++;
+                        cout << "hi" << endl;
+                    }
+                }
+            for (int i =ROWS-1 ; i >= 0; i--){
+                    if (tble [i][j] != 1){
+                         for (int i=ROWS-1; i >= ROWS-TWO; i--){
+                              for(int j=0; j < TWO; j++)  {
+                                   tble [i-counter][j+spot-1] = 1;
+                              }
+                         }
+                    }
+                        //else
+                           // counter++;
+                
+                        
+                        //else
+                            //tble [i-TWO][j+spot-1] = 1;
+                    
+                }
+              //this is full reverse but dont need full reverse
+        /*for (int i = ROWS-1; i  >=0; i--)
+        for(int j = COLS-1; j >= 0; j--)*/
+            
+       //}
 }
